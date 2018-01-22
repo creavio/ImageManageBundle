69c697fe012d08e9f37dc1729206e8ba389c4ab4
malte <malte.frerichs@gmx.net>
2018-01-22 13:42:12 +0000

    - Code Cleanup - Upgrade to PHP 7.1

--- Util/ImageManager.php
+++ Util/ImageManager.php
@@ -1,11 +1,16 @@
 <?php
+
 namespace Creavio\ImageManageBundle\Util;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ImageManager
 {
-	const WEB_FOLDER = 'thumbs';
+	/**
+	 *
+	 */
+	private const WEB_FOLDER = 'thumbs';
+
 	/**
 	 * @var ContainerInterface
 	 */
@@ -24,7 +29,9 @@ class ImageManager
 	 * @param $file
 	 * @param int $width
 	 * @param int $height
+	 *
 	 * @return string
+	 *
 	 * @throws \Exception
 	 */
 	public function resize($type, $file, $width = 0, $height = 0)
@@ -34,11 +41,11 @@ class ImageManager
 		list($outputName, $output) = $this->generateNewFileName($file, $type, $width, $height);
 
 		// When resized object exists dont generate it again, just send output
-		if(file_exists($output)) {
+		if (file_exists($output)) {
 			return $outputName;
 		}
 
-		if($width === null & $height === null) {
+		if ($width === null & $height === null) {
 			// TODO Get Width, height and Mode from config
 			// TODO get versions from config File like small => resize 100x100
 			$mode = 'resize';
@@ -47,7 +54,7 @@ class ImageManager
 		}
 
 		// Check if File exists on file system
-		if(!file_exists($file)) {
+		if (!file_exists($file)) {
 			throw new \Exception('File does not exists');
 		}
 
@@ -55,7 +62,7 @@ class ImageManager
 		list($originalWidth, $originalHeight, $imageType) = getimagesize($file);
 
 		// Get Final image sizes
-		switch($mode) {
+		switch ($mode) {
 			case 'fill':
 				$finalWidth = $width;
 				$finalHeight = $height;
@@ -64,7 +71,7 @@ class ImageManager
 				list($finalWidth, $finalHeight) = $this->getProportionalSize($width, $height, $originalWidth, $originalHeight);
 		}
 
-		switch($imageType) {
+		switch ($imageType) {
 			case IMAGETYPE_JPEG:
 				$image = imagecreatefromjpeg($file);
 				break;
@@ -82,16 +89,16 @@ class ImageManager
 		$imageResized = imagecreatetruecolor($finalWidth, $finalHeight);
 
 		// Handle Transparency
-		if(in_array($imageType, [IMAGETYPE_PNG, IMAGETYPE_GIF])) {
+		if (in_array($imageType, [IMAGETYPE_PNG, IMAGETYPE_GIF])) {
 			$transparency = imagecolortransparent($image);
 			$palletSize = imagecolorstotal($image);
 
-			if($transparency >= 0 && $transparency < $palletSize) {
+			if ($transparency >= 0 && $transparency < $palletSize) {
 				$transparentColor = imagecolorsforindex($image, $transparency);
 				$transparency = imagecolorallocate($imageResized, $transparentColor['red'], $transparentColor['green'], $transparentColor['blue']);
 				imagefill($imageResized, 0, 0, $transparency);
 				imagecolortransparent($imageResized, $transparency);
-			} else if($imageType == IMAGETYPE_PNG) {
+			} else if ($imageType == IMAGETYPE_PNG) {
 				imagealphablending($imageResized, false);
 				$color = imagecolorallocatealpha($imageResized, 0, 0, 0, 127);
 				imagefill($imageResized, 0, 0, $color);
@@ -104,12 +111,12 @@ class ImageManager
 
 		// Check if web folder exists if not create it
 		$completeFolder = $this->container->get('kernel')->getRootDir() . '/../web/' . self::WEB_FOLDER;
-		if(!is_dir($completeFolder)) {
+		if (!is_dir($completeFolder)) {
 			mkdir($completeFolder, 0755);
 		}
 
 
-		switch($imageType) {
+		switch ($imageType) {
 			case IMAGETYPE_GIF:
 				imagegif($imageResized, $output);
 				break;
@@ -132,14 +139,15 @@ class ImageManager
 	 * @param $targetHeight
 	 * @param $originalWidth
 	 * @param $originalHeight
+	 *
 	 * @return array
 	 */
-	private function getProportionalSize($targetWidth, $targetHeight, $originalWidth, $originalHeight)
+	private function getProportionalSize($targetWidth, $targetHeight, $originalWidth, $originalHeight): array
 	{
 		// Get resize factor
-		if($targetWidth === 0) {
+		if ($targetWidth === 0) {
 			$factor = $targetHeight / $originalHeight;
-		} else if($targetHeight === 0) {
+		} else if ($targetHeight === 0) {
 			$factor = $targetWidth / $originalWidth;
 		} else {
 			$factor = min(($targetWidth / $originalWidth), ($targetHeight / $originalHeight));
@@ -155,7 +163,7 @@ class ImageManager
 	 * @param $originalHeight
 	 * @return array
 	 */
-	private function getSizeFromFactor($factor, $originalWidth, $originalHeight)
+	private function getSizeFromFactor($factor, $originalWidth, $originalHeight): array
 	{
 		// Get final sizes from factor
 		$finalWidth = round($originalWidth * $factor);
@@ -169,13 +177,14 @@ class ImageManager
 	 * @param $type
 	 * @param $width
 	 * @param $height
-	 * @return string
+	 *
+	 * @return array
 	 */
-	private function generateNewFileName($originalFile, $type, $width, $height)
+	private function generateNewFileName($originalFile, $type, $width, $height): array
 	{
 		$pathParts = pathinfo($originalFile);
 
-		$newName = self::WEB_FOLDER .'/' . $pathParts['filename'] . '-' . $type . '-' . $width . 'x' . $height . '.' . $pathParts['extension'];
+		$newName = self::WEB_FOLDER . '/' . $pathParts['filename'] . '-' . $type . '-' . $width . 'x' . $height . '.' . $pathParts['extension'];
 		$newPath = $this->container->get('kernel')->getRootDir() . '/../web/' . $newName;
 
 		return [$newName, $newPath];
@@ -183,9 +192,10 @@ class ImageManager
 
 	/**
 	 * @param string $file
+	 *
 	 * @return string
-     */
-	private function fileCleanUp($file)
+	 */
+	private function fileCleanUp($file): string
 	{
 		return ($file[0] == '/') ? $file : '/' . $file;
 	}
